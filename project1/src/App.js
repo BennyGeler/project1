7890783486ac7299e53c1fcd7bbc8ccd4ddfbc03
Benny Geler <bengaler1996@gmail.com>
2020-02-10 15:41:04 +0200

    that is the empty page, from here you can always create new branch and work from zero

--- project1/src/App.js
+++ project1/src/App.js
@@ -4,18 +4,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header>
-        <div className="menu">
-           <ul>
-            <li>Order now</li>
-            <li>Contact us</li>
-            <li>About</li>
-            <li>Join our program</li>
-           </ul>
-        </div>
-      </header>    
-
-
 
     </div>
   );
