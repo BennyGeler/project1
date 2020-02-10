9d3899fd3420ef140b2e3f724f1e9afde47e0fca
Benny Geler <bengaler1996@gmail.com>
2020-02-10 15:31:37 +0200

    add a background image and primary nav bar

---
+++ project1/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div className="menu">
+           <ul>
+            <li>Order now</li>
+            <li>Contact us</li>
+            <li>About</li>
+            <li>Join our program</li>
+           </ul>
+        </div>
+      </header>    
+
+
+
+    </div>
+  );
+}
+
+export default App;
